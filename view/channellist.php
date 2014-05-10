a2b9f9e2b24004d7065071fba0e6ced475cbaa47
sg80 <Stefan@Stefans-MacBook-Air.local>
2014-05-11 01:07:56 +0200

    added download-link

--- view/channellist.php
+++ view/channellist.php
@@ -8,31 +8,52 @@ $channelCollection = $channelFile->getChannelCollection();
 	$(function() {
 		$('.channel-list tr').tsort({attr: 'data-index'});
 		$('.channel-list').tableDnD({
-			onDragClass: 'dragging'
+			onDragClass: 'dragging',
+			onDrop: function(table, row) {
+				renumberChannels();
+			}
+		});
+
+		$('.delete').on('click', function() {
+			return false;
 		});
 
 		$('.delete').on('dblclick', function() {
-			// @todo implement
-			alert("Not yet implemented.");
+			$(this).parentsUntil('tr').parent().fadeOut(function() {
+				$(this).remove();
+				renumberChannels();
+			});
 			return false;
 		});
 	});
+
+	function renumberChannels() {
+		var i = 1;
+
+		$('.channel-list .index').each(function() {
+			$(this).text(i++);
+		});
+	}
 </script>
+
+<div class="download">
+	<a href="#">apply changes and download .scm-file</a>
+</div>
 
 <div class="lists-container">
 	<? for ($i = 0; $i < 1; $i++) { // idea is to show air, cable and sat-lists here ?>
-	<table class="channel-list">
-		<? foreach($channelCollection as $channel) { ?>
-			<tr data-index="<?=$channel->getIndex()?>">
-				<td class="index"><?=$channel->getIndex()?></td>
-				<td class="service-type"><div class="service-type service-type-<?=$channel->getServiceType()?>"><span class="invisible"><?=$channel->getServiceTypeName()?></span></td>
-				<td class="logo logo-<?=$channel->getLogoFileName()?>"></td>
-				<td class="name"><?=utf8_encode($channel->getName())?></td>
-				<td class="options">
-					<a class="delete" href="#" title="double-click"><span class="invisible">delete</span></a>
-				</td>
-			</tr>
-		<? } ?>
-	</table>
+		<table class="channel-list">
+			<? foreach($channelCollection as $channel) { ?>
+				<tr data-index="<?=$channel->getIndex()?>">
+					<td class="index"><?=$channel->getIndex()?></td>
+					<td class="service-type"><div class="service-type service-type-<?=$channel->getServiceType()?>"><span class="invisible"><?=$channel->getServiceTypeName()?></span></td>
+					<td class="logo logo-<?=$channel->getLogoFileName()?>"></td>
+					<td class="name"><?=utf8_encode($channel->getName())?></td>
+					<td class="options">
+						<a class="delete" href="#" title="double-click"><span class="invisible">delete</span></a>
+					</td>
+				</tr>
+			<? } ?>
+		</table>
 	<? } ?>
 </div>
